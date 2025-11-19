7cad2270cd3047fc9a4ae95545cbdc570820b527
David Caldwell <davidjuliancaldwell@gmail.com>
2025-11-19 07:59:23 -0800

    working on raincloud type plots, fig 2 working nicely, fig 3 not yet

--- mtbi_cost_effective.r
+++ mtbi_cost_effective.r
@@ -4,6 +4,7 @@ library(ggplot2)
 library(ggbeeswarm)
 library(janitor)
 library(here)
+library(ggrain)
 
 ########
 
@@ -60,6 +61,10 @@ data_fig_3 <-   data_fig_3 %>%
       factor(levels = (c("civilian","military"))
   ))
 
+data_fig_3 <- data_fig_3 %>%
+mutate(
+  study = as.factor(study)
+)
 
 ### figure 2
 
@@ -75,9 +80,6 @@ fig_2 <- ggplot(data_fig_2, aes(x = time, y = costs, color = group,size = n)) +
   ) +
       theme_minimal() +
     scale_color_hue(h = c(180, 300))
-
-
-
 
 ### figure 3
 
@@ -100,3 +102,42 @@ dodge_position <- position_dodge(width = 0.5)
 
 fig_2
 fig_3
+
+########
+
+# raincloud plots
+
+### figure 2
+
+  fig_2_rain <- ggplot(data_fig_2, aes(x = time, y = costs, fill = group)) +
+    geom_rain(alpha = 0.5, point.args = list(alpha = 0)) +         # stat layer — don't map size here
+    geom_beeswarm(aes(size = n,color = group), dodge.width = 0.1, alpha = 0.7) +
+    theme_classic() +
+    scale_fill_hue(h = c(180, 300)) +
+    scale_color_hue(h = c(180, 300)) +
+    labs( 
+      title = "Costs/Charges Reported for mTBI Stratified by Year ",
+      subtitle = "Each point is one study",
+      x = "Year of Study",
+      y = "Amount (2025 USD Dollars)",
+      size = "Number of Subjects"
+    )
+  ### figure 3
+
+dodge_position <- position_dodge(width = 0.5)
+
+ fig_3_rain <-  ggplot(data_fig_3, aes(x = time, y = amount, fill = group)) +
+      geom_rain(alpha=0.5,point.args=list(drop=FALSE)) + # Use dodge.width to avoid overlap
+  labs(
+    title = "Costs/Charges Reported for mTBI Stratified by Year ",
+    subtitle = "Each point is one study",
+    x = "Year of Study",
+    y = "Amount (2025 USD Dollars)",
+    color = "Costs/Charges",
+    size = "Number of Subjects"
+  )  +
+      theme_classic()
+
+
+fig_2_rain
+fig_3_rain
